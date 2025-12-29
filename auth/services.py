287d49d7125ef73b9602c8f3e4ab0bd46a3aef5c
Mustafa SAYDAN <mustafasaydan16@gmail.com>
2025-12-29 16:29:54 +0300

    feat: add basic user registration logic

---
+++ auth/services.py
@@ -0,0 +1,27 @@
+from flask import current_app
+from extensions import db,bcrypt
+from models import Kullanici
+import jwt
+import datetime
+
+def kullanici_kayit(data):
+    kullanici_adi=data.get("kullanici_adi")
+    email=data.get("email")
+    sifre=data.get("sifre")
+
+    if Kullanici.query.filter_by(kullanici_adi=kullanici_adi).first():
+        return {"hata":"Bu kullanıcı adı zaten kullanılıyor"},400
+
+    sifre_hash=bcrypt.generate_password_hash(sifre).decode("utf-8")
+
+    yeni_kullanici = Kullanici(
+        kullanici_adi=kullanici_adi,
+        email=email,
+        sifre=sifre_hash
+    )
+
+    db.session.add(yeni_kullanici)
+    db.session.commit()
+
+    return{"mesaj":"Kullanici başarıyla kaydedildi"},201
+
